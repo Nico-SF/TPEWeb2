f0eddfa219f2c7508cd25e4b29d8ef9b77e8d57c
Nicolas Sanchez <nicosanchezfiadone@gmail.com>
2023-10-18 23:20:49 -0300

    Admin + LogIn + LogOut

---
+++ app/controllers/admin.controller.php
@@ -0,0 +1,105 @@
+<?php
+    require_once './app/models/admin.model.php';
+    require_once './app/views/admin.view.php';
+    require_once './app/helpers/auth.helper.php';
+
+    class AdminController {
+        private $model;
+        private $view;
+
+        public function __construct() {
+            $this->model = new AdminModel();
+            $this->view = new AdminView();
+        }
+
+        public function debugConsole($data) {
+        $output = $data;
+        if (is_array($output))
+            $output = implode(',', $output);
+
+        echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+        }
+        public function showAdmin() {
+        AuthHelper::init();
+        $this->debugConsole(isset($_SESSION['USER_ID']) ? $_SESSION["USER_ID"] : "NO ESTÄ");
+            if (isset($_SESSION['USER_ID'])) {
+                $products = $this->model->getProducts();
+                $categories = $this->model->getCategories();
+                $this->view->showAdmin($products, $categories);
+            } else {
+                $this->showLogin();
+            }
+        }
+
+        public function showLogin() {
+            $this->view->showLogin();
+        }
+
+        public function login() {
+            $user = $this->model->getByUsername($_POST["username"]);
+
+            if ($user && password_verify($_POST["password"], $user->usuario_pass)) {
+                AuthHelper::login($user);
+                header('Location: ' . BASE_URL .'admin');
+            } else {
+                $this->view->showError("Error en la autenticación!");
+            }
+        }
+
+        public function logout() {
+            AuthHelper::logout();
+        }
+
+        public function password_verify() {}
+
+        // CRUD
+
+        // Product
+        public function crearProducto() {
+            AuthHelper::verify();
+            $category_id = $_POST['product_category'];
+            $product_name = $_POST['product_name'];
+            $description = $_POST['description'];
+            $price = $_POST['price'];
+            $this->model->createProduct($category_id, $product_name, $description, $price);
+            header('Location: ' . BASE_URL .'admin');
+        }
+
+        public function editarProducto($product_id) {
+            AuthHelper::verify();
+            $category_id = $_POST['product_category'];
+            $product_name = $_POST['product_name'];
+            $description = $_POST['description'];
+            $price = $_POST['price'];
+            $this->model->updateProduct($product_id, $category_id, $product_name, $description, $price);
+            header('Location: ' . BASE_URL .'admin');
+        }
+
+        public function borrarProducto($product_id) {
+            AuthHelper::verify();
+            $this->model->deleteProduct($product_id);
+            header('Location: ' . BASE_URL .'admin');
+        }
+
+        // Category
+        public function crearCategoria() {
+            AuthHelper::verify();
+            $category_name = $_POST['category_name'];
+            $this->model->createCategory($category_name);
+            header('Location: ' . BASE_URL .'admin');
+        }
+
+        public function editarCategoria($category_id) {
+            AuthHelper::verify();
+            $category_name = $_POST['category_name'];
+            $this->model->updateCategory($category_id, $category_name);
+            header('Location: ' . BASE_URL .'admin');
+        }
+
+        public function borrarCategoria($categoryId) {
+            AuthHelper::verify();
+            $this->model->deleteCategory($categoryId);
+            header('Location: ' . BASE_URL .'admin');
+        }
+    }
+?>
